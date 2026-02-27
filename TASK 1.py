b4681daedc7668d05f52ac6dcb68e3e8724e5f1d
Deadcalm-code <jayneetsingh4@gmail.com>
2026-02-28 03:40:31 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,10 @@
+Num1=int(input('Enter the first number: '))
+Num2=int(input('Enter the second number: '))
+Addition=Num1+Num2
+Subtraction=Num1-Num2
+Multiplication=Num1*Num2
+Division=Num1/Num2
+print('Addition:',Addition)
+print('Subtraction:',Subtraction)
+print('Multiplication:',Multiplication)
+print('Division:',Division)
